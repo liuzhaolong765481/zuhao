ee7e2baff50a2c9a1908ec66c4db756d012176fe
lzl <1552519081>
2020-10-24 21:23:33 +0800

    前台首页

--- resources/views/tpl/index.blade.php
+++ resources/views/tpl/index.blade.php
@@ -1,5 +1,5 @@
 @include('tpl._include.header')
 <div style="height: 500px;">
-    <h1 style="margin-top:300px;text-align: center">内容明天放！！！！！！！！！！！！！！！！！</h1>
+    <h1 style="margin-top:300px;text-align: center">内容明天放！！！！！！！！！！！！！</h1>
 </div>
 @include('tpl._include.footer')
