a5d08bf1d1de8408be137ca654d38f374fb7a698
lzl <1552519081>
2020-10-24 22:09:31 +0800

    前台首页

--- resources/views/template/404.blade.php
+++ resources/views/template/404.blade.php
@@ -1,8 +1,17 @@
-@include('tpl._include.header')
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>飞讯租号</title>
+    <link type="text/css" rel="stylesheet" href="{{asset('css/public.css')}}"/>
+</head>
+<body>
 <div class="error404">
     <img src="{{asset('images/404.png')}}" alt="">
     <span>很抱歉，页面它不小心迷路了~~</span>
     <a href="/">返回首页</a>
 </div>
-@include('tpl._include.footer')
-
+</body>
+</html>
--- resources/views/template/500.blade.php
+++ resources/views/template/500.blade.php
@@ -1,7 +1,17 @@
-@include('tpl._include.header')
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>飞讯租号</title>
+    <link type="text/css" rel="stylesheet" href="{{asset('css/public.css')}}"/>
+</head>
+<body>
 <div class="error404 center">
     <img src="{{asset('images/500.png')}}" alt="">
     <span>抱歉，您访问的页面有误~</span>
     <a href="/">返回首页</a>
 </div>
-@include('tpl._include.footer')
+</body>
+</html>
