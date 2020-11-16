89f1e14bd34d39d6614c90e028fc9f19c7c82069
voygael <1552519081@qq.com>
2020-11-16 11:10:55 +0800

    租号大厅

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
 <?php
-/**
- * Created by lzl
- * Date: 2020 2020/11/3
- * Time: 11:27
- */
-
-$rules = [
-    'avatar'         => null,
-    'nick_name'      => null,
-    'email'          => null,
-    'trade_password' => 12,
-    'ali_number'     => null,
-];
-
-var_dump(array_filter($rules));
